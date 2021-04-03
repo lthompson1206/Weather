9e7cd9ba46dca9eadd136e9c934ba73c3694ef58
CodeWorld3 <lthompson1206@gmail.com>
2021-04-03 14:22:09 -0400

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,7 @@
-console.log('client side logic is loaded');
-
-
-
-// fetch('http://localhost:3000/weather?address=philadelphia').then((response) => {
-// response.json().then((data) => {
-//     if (data.error) {
-//         console.log(data.error)
-//     } else {
-//        console.log(data.location)
-//        console.log(data.forecast)
-//     }
-
-// })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -31,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 response.json().then((data) => {
     if (data.error) {
       messageOne.textContext = data.error
